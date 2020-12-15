40a30e5119bb87d83292859696f08a695f36746a
Alexandre Bulcao <ambulcao@gmail.com>
2020-12-15 12:35:47 +0000

    Ajustando mensagens do Bot

--- index.js
+++ index.js
@@ -7,16 +7,45 @@ if(ProcessingInstruction.env.bot_KEY){
 
 const app = new Discord.Client();
 
+let estouPronto = false;
+
 app.on('ready', () => {
     console.log('Estou conectado!');
 });
 
-app.on('message', () => {
+app.on('message', (msg) => {
     // !join = Bot se junta ao canal de voz
-    
-    // !leave = Bot sai do canal de voz
+    if(msg.content === '!join'){
+        if(msg.member.voiceChannel){
+            msg.member.voiceChannel.join();
+            estouPronto = true;
+            // verifica de a pessoa esta no canal de voz
 
+        } else {
+            // caso nao esteja em um 
+            msg.channel.send('Você precisa estar conectado a um Canal de Voz!');
+        }
+    }
+    // !leave = Bot sai do canal de voz
+    else if(msg.content === '!leave'){
+            if(msg.member.voiceChannel){
+                msg.member.voiceChannel.leave();
+                estouPronto = false;    
+            } else {
+                msg.channel.send('Você precisa estar conectado a um Canal de Voz!');
+            }
+    }
     // !play [link] = Bot toca músicas
+    else if(msg.content.startsWith('!play ')){
+        if(estouPronto){
+            let oQTocar = msg.content.replace('!play ','');
+            if(Ytdl.validateURL(oQTocar)){
+                msg.member.voiceChannel.connection.playStream(Ytdl(oQTocar));
+            } else {
+                msg.channel.send('O link não é válido!');
+            }
+        }
+    }
 });
 
 app.login();
